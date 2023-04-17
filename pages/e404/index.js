4754b1ddf36a39a69e542f88c7153a9445e89630
chosenchilde <edencyberspace@gmail.com>
2023-04-17 20:46:13 -0300

    Db Json update

--- pages/e404/index.js
+++ pages/e404/index.js
@@ -1,5 +1,11 @@
 $(document).ready(myE404)
 
-function myE404(){
+/**
+ * Função principal da página "e404".
+ **/
+function myE404() {
+    /**
+     * Altera o título da página quando 'e404' for acessada.
+     **/
     changeTitle('Erro 404')
 }
